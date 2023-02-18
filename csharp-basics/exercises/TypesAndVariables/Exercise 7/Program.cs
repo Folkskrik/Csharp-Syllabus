949cb615d827c20c85fa7647338a761482af993d
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-02-18 16:52:02 +0200

    i gues

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise 7/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string input;
+        int count, i, len;
+
+        Console.Write("Enter a string: ");
+        input = Console.ReadLine();
+
+        count = 0;
+        len = input.Length;
+
+        for (i = 0; i < len; i++)
+        {
+            if (Char.IsUpper(input[i]))
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("The number of uppercase letters in the string is: " + count);
+    }
+}
