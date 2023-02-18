c9a50c0145d835d80e3661619e5572edf0342e74
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-02-18 18:50:02 +0200

    Done?

--- csharp-basics/exercises/TypesAndVariables/Casting/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Casting/Program.cs
@@ -1,25 +1,30 @@
 ﻿using System;
 
-static void First()
+namespace Casting
 {
-    string a = "1";
-    int b = 2;
-    int c = 3;
-    double d = 4;
-    float e = 5;
+    class Program
+    {
+        static void First()
+        {
+            string a = "1";
+            int b = 2;
+            int c = 3;
+            double d = 4;
+            float e = 5;
 
-    int sum = int.Parse(a) + b + c + (int)d + (int)e;
-    Console.WriteLine(sum);
-}
+            int sum = int.Parse(a) + b + c + (int)d + (int)e;
+            Console.WriteLine(sum);
+        }
+        static void Second()
+        {
+            string a = "1";
+            int b = 2;
+            int c = 3;
+            double d = 4.2;
+            float e = 5.3f;
 
-static void Second()
-{
-    string a = "1";
-    int b = 2;
-    int c = 3;
-    double d = 4.2;
-    float e = 5.3f;
-
-    double sum = double.Parse(a) + b + c + d + e;
-    Console.WriteLine(sum);
+            double sum = double.Parse(a) + b + c + d + e;
+            Console.WriteLine(sum);
+        }
+    }
 }
