6bfe2b979307eaa903b83b599eec197f66404eef
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-02-18 17:53:48 +0200

    Izlabots

--- csharp-basics/exercises/TypesAndVariables/Exercise 9/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Exercise 9/Program.cs
@@ -1,27 +1,35 @@
-﻿class Program
+﻿
+class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
+        // Input distance in meters
         Console.Write("Enter distance in meters: ");
         double distance = double.Parse(Console.ReadLine());
 
-        Console.Write("Enter time in hours: ");
-        double hours = double.Parse(Console.ReadLine());
-        Console.Write("Enter time in minutes: ");
-        double minutes = double.Parse(Console.ReadLine());
-        Console.Write("Enter time in seconds: ");
-        double seconds = double.Parse(Console.ReadLine());
+        // Input time in hours, minutes, and seconds
+        Console.Write("Enter time taken (hours): ");
+        int hours = int.Parse(Console.ReadLine());
+        Console.Write("Enter time taken (minutes): ");
+        int minutes = int.Parse(Console.ReadLine());
+        Console.Write("Enter time taken (seconds): ");
+        int seconds = int.Parse(Console.ReadLine());
 
-        double totalTimeInSeconds = (hours * 3600) + (minutes * 60) + seconds;
+        // Convert time to total seconds
+        int totalSeconds = hours * 3600 + minutes * 60 + seconds;
 
-        double speedInMetersPerSecond = distance / totalTimeInSeconds;
+        // Calculate speed in meters per second
+        double speedMps = distance / totalSeconds;
 
-        double speedInKmPerHour = (distance / 1000.0) / (totalTimeInSeconds / 3600.0);
+        // Calculate speed in kilometers per hour
+        double speedKph = (distance / 1000.0) / (totalSeconds / 3600.0);
 
-        double speedInMilesPerHour = (distance / 1609.0) / (totalTimeInSeconds / 3600.0);
+        // Calculate speed in miles per hour
+        double speedMph = (distance / 1609.0) / (totalSeconds / 3600.0);
 
-        Console.WriteLine("Your speed in meters/second is: " + speedInMetersPerSecond);
-        Console.WriteLine("Your speed in km/h is: " + speedInKmPerHour);
-        Console.WriteLine("Your speed in miles/h is: " + speedInMilesPerHour);
+        // Output results
+        Console.WriteLine($"Your speed in meters/second is {speedMps:F8}");
+        Console.WriteLine($"Your speed in km/h is {speedKph:F8}");
+        Console.WriteLine($"Your speed in miles/h is {speedMph:F8}");
     }
 }
