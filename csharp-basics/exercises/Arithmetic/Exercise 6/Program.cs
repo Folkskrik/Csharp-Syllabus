84219fee09584b9550d54e359edf0ec6ab2cbed5
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-02-19 17:26:03 +0200

    Exercise 6

---
+++ csharp-basics/exercises/Arithmetic/Exercise 6/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace CozaLozaWoza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int lowerBound = 1;
+            const int upperBound = 110;
+            const int numbersPerLine = 11;
+
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.Write("Coza");
+                }
+                if (i % 5 == 0)
+                {
+                    Console.Write("Loza");
+                }
+                if (i % 7 == 0)
+                {
+                    Console.Write("Woza");
+                }
+                if (i % 3 != 0 && i % 5 != 0 && i % 7 != 0)
+                {
+                    Console.Write(i);
+                }
+
+                if (i % numbersPerLine == 0)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
