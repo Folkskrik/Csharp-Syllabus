7820e26a63262f75b307120c1359c4b6e00c4e05
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-02-19 16:54:15 +0200

    Exercise 4

---
+++ csharp-basics/exercises/Arithmetic/Exercise 4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Product1ToN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int N = 10;
+            int product = 1;
+
+            for (int i = 1; i <= N; i++)
+            {
+                product *= i;
+            }
+
+            Console.WriteLine("The product of integers from 1 to {0} is: {1}", N, product);
+        }
+    }
+}
