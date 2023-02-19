b4b6436b421db012bda6408315908804aa761674
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-02-19 15:10:08 +0200

    Exercise 1

---
+++ csharp-basics/exercises/Arithmetic/Exercise 1/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int num1, num2, sum, diff;
+
+        Console.Write("Enter the first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        sum = num1 + num2;
+        diff = num1 - num2;
+
+        if (num1 == 15 || num2 == 15 || sum == 15 || diff == 15 || diff == -15)
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+
+        Console.ReadLine();
+    }
+}
