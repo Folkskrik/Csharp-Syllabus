5ba1b7349301132764b3d056d2ddf016ab73db54
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-02-19 15:24:17 +0200

    Exercise 2

---
+++ csharp-basics/exercises/Arithmetic/Exercise 2/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int number;
+
+        Console.Write("Enter an integer: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Even Number");
+        }
+        else
+        {
+            Console.WriteLine("Odd Number");
+        }
+
+        Console.WriteLine("bye!");
+        Console.ReadLine();
+    }
+}
