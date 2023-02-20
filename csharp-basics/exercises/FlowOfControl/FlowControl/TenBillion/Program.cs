a820ad38dbfc45f8057589bd52e92247166021fa
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-02-20 19:15:29 +0200

    TenBillion

--- csharp-basics/exercises/FlowOfControl/FlowControl/TenBillion/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/TenBillion/Program.cs
@@ -4,64 +4,60 @@ namespace TenBillion
 {
     class Program
     {
-        //TODO Write a C# program that reads an positive integer and count the number of digits the number (less than ten billion) has.
         static void Main(string[] args)
         {
             Console.WriteLine("Input an integer number less than ten billion: ");
 
             var input = Console.ReadLine();
 
-            /*
-            todo - check if number
-            if (?) 
+            long n;
+            if (long.TryParse(input, out n))
             {
-                //todo - check if n is less than zero
-                if (?) 
+                if (n < 0) 
                 {
                     n *= -1;
                 }
 
-                fixme
-                if (?) 
+                if (n >= 10000000000) 
                 {
                     Console.WriteLine("Number is greater or equals 10,000,000,000!");
                 } 
                 else 
                 {
                     int digits = 1;
-                    if (?) 
+                    if (n >= 10) 
                     {
                         digits = 2;
                     } 
-                    else if (?) 
+                    if (n >= 100) 
                     {
                         digits = 3;
                     } 
-                    else if (?) 
+                    if (n >= 1000) 
                     {
                         digits = 4;
                     } 
-                    else if (?) 
+                    if (n >= 10000) 
                     {
                         digits = 5;
                     } 
-                    else if (?) 
+                    if (n >= 100000) 
                     {
                         digits = 6;
                     } 
-                    else if (?) 
+                    if (n >= 1000000) 
                     {
                         digits = 7;
                     } 
-                    else if (?) 
+                    if (n >= 10000000) 
                     {
                         digits = 8;
                     } 
-                    else if (?) 
+                    if (n >= 100000000) 
                     {
                         digits = 9;
                     } 
-                    else if (?) 
+                    if (n >= 1000000000) 
                     {
                         digits = 10;
                     }
@@ -73,7 +69,6 @@ namespace TenBillion
             {
                 Console.WriteLine("The number is not a long");
             }
-            */
         }
     }
 }
