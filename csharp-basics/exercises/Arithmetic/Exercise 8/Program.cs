b4e428d643482511415ea4800e8435c86bbb8eb0
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-02-22 18:17:33 +0200

    Updated Ex8

--- csharp-basics/exercises/Arithmetic/Exercise 8/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise 8/Program.cs
@@ -1,37 +1,39 @@
-﻿decimal salary1;
-decimal salary2;
-decimal salary3;
+﻿using System;
 
-int hours1;
-int hours2;
-int hours3;
-
-salary1 = 7.50m;
-salary2 = 8.20m;
-salary3 = 10.00m;
-
-hours1 = 35;
-hours2 = 47;
-hours3 = 73;
-
-Console.WriteLine($"Employee1: {CalculateSalary(salary1, hours1)}");
-Console.WriteLine($"Employee2: {CalculateSalary(salary2, hours2)}");
-Console.WriteLine($"Employee3: {CalculateSalary(salary3, hours3)}");
-
-static string CalculateSalary (decimal basicPay, int hoursWorked)
+class Program
 {
-    if (basicPay < 8.00m || hoursWorked > 60)
+    static void Main(string[] args)
     {
-        return "error";
+        decimal salary1 = 7.50m;
+        decimal salary2 = 8.20m;
+        decimal salary3 = 10.00m;
+
+        int hours1 = 35;
+        int hours2 = 47;
+        int hours3 = 73;
+
+        Console.WriteLine($"Employee1: {CalculateSalary(salary1, hours1)}");
+        Console.WriteLine($"Employee2: {CalculateSalary(salary2, hours2)}");
+        Console.WriteLine($"Employee3: {CalculateSalary(salary3, hours3)}");
+
+        Console.ReadLine();
     }
-    if (hoursWorked <= 40)
+
+    static string CalculateSalary(decimal basicPay, int hoursWorked)
     {
-        return (hoursWorked + basicPay).ToString();
+        if (basicPay < 8.00m || hoursWorked > 60)
+        {
+            return "error";
+        }
+        if (hoursWorked <= 40)
+        {
+            return (hoursWorked + basicPay).ToString();
+        }
+
+        decimal salary = 40 * basicPay;
+
+        salary += (hoursWorked - 40) * (basicPay * 1.5m);
+
+        return salary.ToString();
     }
-
-    var salary = 40 * basicPay;
-
-    salary += (hoursWorked - 40) * (basicPay * 1.5m);
-
-    return salary.ToString();
 }
