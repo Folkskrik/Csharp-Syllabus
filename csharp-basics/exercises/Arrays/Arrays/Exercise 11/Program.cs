c1dff0df75a1b99730b89ada2eeda3d3d6b84a17
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-02-26 19:09:58 +0200

    Exercise 11

---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise 11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string sentence = "I am loking for someone called: Nemo !";
+        Console.WriteLine(FindNemo(sentence));
+
+        string sentence2 = "I am not finding Nemo's tracks !";
+        Console.WriteLine(FindNemo(sentence2));
+
+        string sentence3 = "I did find Nemo even if Nemo didn't want it !";
+        Console.WriteLine(FindNemo(sentence3));
+    }
+
+    public static string FindNemo(string sentence)
+    {
+        string[] words = sentence.Split(' ');
+        int index = Array.IndexOf(words, "Nemo");
+        if (index != -1)
+        {
+            return $"I found Nemo at {index + 1}!";
+        }
+        return "I can't find Nemo :(";
+    }
+}
