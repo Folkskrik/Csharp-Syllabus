1c3a769116d7106d433930bffa0a80bfc5865835
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-02-28 23:45:39 +0200

    Exercise 8

---
+++ csharp-basics/exercises/Loops/Loops/Exercise 8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class AsciiFigure
+{
+    static void Main()
+    {
+        Console.Write("Enter the size of the figure: ");
+        int size = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size * 4 - 4 - i * 4; j++)
+            {
+                Console.Write("/");
+            }
+            for (int j = 0; j < i * 8; j++)
+            {
+                Console.Write("*");
+            }
+            for (int j = 0; j < size * 4 - 4 - i * 4; j++)
+            {
+                Console.Write("\\");
+            }
+            Console.WriteLine();
+        }
+    }
+}
