31ba9dd2ba2fa1e95b25f9681793d341f5f60bb1
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-02-28 16:23:49 +0200

    Exercise 5

---
+++ csharp-basics/exercises/Loops/Loops/Exercise 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first word:");
+            string firstWord = Console.ReadLine();
+
+            Console.WriteLine("Enter second word:");
+            string secondWord = Console.ReadLine();
+
+            int dotCount = 30 - firstWord.Length - secondWord.Length;
+            string dots = "";
+            for (int i = 0; i < dotCount; i++)
+            {
+                dots += ".";
+            }
+
+            string output = firstWord + dots + secondWord;
+            Console.WriteLine(output);
+
+            Console.ReadKey();
+        }
+    }
+}
