9647ef071ad4048c09199e4c21080ae9eff4db1d
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-03-04 22:14:13 +0200

    Exercise 1

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 1/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Product
+{
+    private string name;
+    private double price;
+    private int amount;
+
+    public Product(string name, double priceAtStart, int amountAtStart)
+    {
+        this.name = name;
+        this.price = priceAtStart;
+        this.amount = amountAtStart;
+    }
+
+    public void PrintProduct()
+    {
+        Console.WriteLine($"{name}, price {price}, amount {amount}");
+    }
+
+    public void ChangeQuantity(int newQuantity)
+    {
+        this.amount = newQuantity;
+    }
+
+    public void ChangePrice(double newPrice)
+    {
+        this.price = newPrice;
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Product mouse = new Product("Logitech mouse", 70.00, 14);
+        Product iphone = new Product("iPhone 5s", 999.99, 3);
+        Product projector = new Product("Epson EB-U05", 440.46, 1);
+
+        mouse.PrintProduct();
+        iphone.PrintProduct();
+        projector.PrintProduct();
+
+        mouse.ChangeQuantity(20);
+        iphone.ChangePrice(899.99);
+
+        mouse.PrintProduct();
+        iphone.PrintProduct();
+    }
+}
