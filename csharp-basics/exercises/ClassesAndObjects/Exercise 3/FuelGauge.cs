5060f41c24877670d1d28ebf2b1780e3facce322
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-03-05 01:46:45 +0200

    Pareizais EX3

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 3/FuelGauge.cs
@@ -0,0 +1,63 @@
+﻿class FuelGauge
+{
+    private int fuel;
+
+    public FuelGauge()
+    {
+        fuel = 0;
+    }
+
+    public int GetFuel()
+    {
+        return fuel;
+    }
+
+    public void IncrementFuel()
+    {
+        if (fuel < 70)
+        {
+            fuel++;
+        }
+    }
+
+    public void DecrementFuel()
+    {
+        if (fuel > 0)
+        {
+            fuel--;
+        }
+    }
+}
+
+class Odometer
+{
+    private int mileage;
+    private FuelGauge fuelGauge;
+
+    public Odometer(FuelGauge fuelGauge)
+    {
+        mileage = 0;
+        this.fuelGauge = fuelGauge;
+    }
+
+    public int GetMileage()
+    {
+        return mileage;
+    }
+
+    public void IncrementMileage()
+    {
+        if (mileage < 999999)
+        {
+            mileage++;
+        }
+        else
+        {
+            mileage = 0;
+        }
+        if (mileage % 10 == 0)
+        {
+            fuelGauge.DecrementFuel();
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 3/Program.cs
@@ -0,0 +1,16 @@
+﻿FuelGauge fuelGauge = new FuelGauge();
+Odometer odometer = new Odometer(fuelGauge);
+
+while (fuelGauge.GetFuel() < 70)
+{
+    fuelGauge.IncrementFuel();
+}
+
+while (fuelGauge.GetFuel() > 0)
+{
+    odometer.IncrementMileage();
+    Console.WriteLine("Mileage: " + odometer.GetMileage() + " km");
+    Console.WriteLine("Fuel: " + fuelGauge.GetFuel() + " L");
+}
+
+Console.WriteLine("The car is out of fuel.");
