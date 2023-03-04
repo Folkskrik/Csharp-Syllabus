0e26455f5f8fe1da190119b8907790bc33bba056
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-03-05 01:35:07 +0200

    FuelConsumptionCalculator(Sajaucu kur rakst

--- csharp-basics/exercises/ClassesAndObjects/FuelConsumptionCalculator/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/FuelConsumptionCalculator/Program.cs
@@ -8,29 +8,52 @@ namespace FuelConsumptionCalculator
 {
     class Program
     {
-        private static void Main(string[] args)
+        static void Main()
         {
-            FuelGauge fuelGauge = new FuelGauge();
-            Odometer odometer = new Odometer(fuelGauge);
+            Console.WriteLine("Enter data for Car 1:");
+            Console.Write("Start kilometers: ");
+            double startKilometers1 = double.Parse(Console.ReadLine());
+            Console.Write("End kilometers: ");
+            double endKilometers1 = double.Parse(Console.ReadLine());
+            Console.Write("Liters of gas used: ");
+            double liters1 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Filling up the car...");
-            for (int i = 0; i < 70; i++)
+            Car car1 = new Car(startKilometers1, endKilometers1, liters1);
+
+            Console.WriteLine("Enter data for Car 2:");
+            Console.Write("Start kilometers: ");
+            double startKilometers2 = double.Parse(Console.ReadLine());
+            Console.Write("End kilometers: ");
+            double endKilometers2 = double.Parse(Console.ReadLine());
+            Console.Write("Liters of gas used: ");
+            double liters2 = double.Parse(Console.ReadLine());
+
+            Car car2 = new Car(startKilometers2, endKilometers2, liters2);
+
+            Console.WriteLine("Car 1 consumption: {0:F2} L/100km", car1.CalculateConsumption());
+            Console.WriteLine("Car 2 consumption: {0:F2} L/100km", car2.CalculateConsumption());
+
+            if (car1.GasHog())
             {
-                fuelGauge.IncrementFuel();
+                Console.WriteLine("Car 1 is a gas hog");
+            }
+            else if (car1.EconomyCar())
+            {
+                Console.WriteLine("Car 1 is an economy car");
             }
 
-            Console.WriteLine("Starting the journey...");
-            while (fuelGauge.GetCurrentFuel() > 0)
+            if (car2.GasHog())
             {
-                odometer.IncrementMileage();
-
-                Console.WriteLine("Current mileage: " + odometer.GetCurrentMileage() + " km");
-                Console.WriteLine("Current fuel: " + fuelGauge.GetCurrentFuel() + " L");
-                Console.WriteLine();
+                Console.WriteLine("Car 2 is a gas hog");
+            }
+            else if (car2.EconomyCar())
+            {
+                Console.WriteLine("Car 2 is an economy car");
             }
 
-            Console.WriteLine("The car has run out of fuel.");
-            Console.ReadKey();
+            Console.WriteLine("Filling up Car 1...");
+            car1.FillUp(10000, 50);
+            Console.WriteLine("Car 1 new consumption: {0:F2} L/100km", car1.CalculateConsumption());
         }
     }
 }
