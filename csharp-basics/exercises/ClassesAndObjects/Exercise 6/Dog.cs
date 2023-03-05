9996176e07b502a90e45be1e28707ec35aca7210
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-03-05 02:34:17 +0200

    Exercise 6

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 6/Dog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Dog
+{
+    public string Name { get; set; }
+    public string Sex { get; set; }
+    public Dog Mother { get; set; }
+    public Dog Father { get; set; }
+
+    public Dog(string name, string sex)
+    {
+        Name = name;
+        Sex = sex;
+    }
+
+    public string FathersName()
+    {
+        return Father == null ? "Unknown" : Father.Name;
+    }
+
+    public bool HasSameMotherAs(Dog otherDog)
+    {
+        if (Mother == null || otherDog.Mother == null)
+        {
+            return false;
+        }
+        else if (Mother.Name == otherDog.Mother.Name)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class DogTest
+{
+    static void Main(string[] args)
+    {
+        Dog max = new Dog("Max", "male");
+        Dog rocky = new Dog("Rocky", "male");
+        Dog sparky = new Dog("Sparky", "male");
+        Dog buster = new Dog("Buster", "male");
+        Dog sam = new Dog("Sam", "male");
+        Dog lady = new Dog("Lady", "female");
+        Dog molly = new Dog("Molly", "female");
+        Dog coco = new Dog("Coco", "female");
+
+        max.Mother = lady;
+        max.Father = rocky;
+
+        coco.Mother = molly;
+        coco.Father = buster;
+
+        rocky.Mother = molly;
+        rocky.Father = sam;
+
+        buster.Mother = lady;
+        buster.Father = sparky;
+
+        Console.WriteLine("Fathers Name:");
+        Console.WriteLine("Coco's father's name: " + coco.FathersName());
+        Console.WriteLine("Sparky's father's name: " + sparky.FathersName());
+
+        Console.WriteLine("\nHas Same Mother:");
+        bool hasSameMother = coco.HasSameMotherAs(rocky);
+        Console.WriteLine("Coco and Rocky have the same mother: " + hasSameMother);
+    }
+}
