d6b63d97cf3d179e461fb8d669dbb8087385d8e6
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-03-05 17:39:16 +0200

    BankAccount

---
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/BankAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BankAccount
+{
+    class BankAccount
+    {
+        private string name;
+        private decimal balance;
+
+        public BankAccount(string name, decimal balance)
+        {
+            this.name = name;
+            this.balance = balance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                throw new Exception("Insufficient funds");
+            }
+        }
+
+        public string ShowUserNameAndBalance()
+        {
+            string balanceString = balance.ToString("0.00");
+
+            if (balance < 0)
+            {
+                balanceString = "-" + "$" + (-balance).ToString("0.00");
+            }
+            else
+            {
+                balanceString = "$" + balanceString;
+            }
+
+            return name + ", " + balanceString;
+        }
+    }
+}
