27a72c8acd9a50e853b7682faebdc6b36bc7e2ca
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-03-05 02:06:38 +0200

    Exercise 4

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 4/Movie.cs
@@ -0,0 +1,33 @@
+﻿class Movie
+{
+    public string title;
+    public string studio;
+    public string rating;
+
+    // Constructor 1
+    public Movie(string title, string studio, string rating)
+    {
+        this.title = title;
+        this.studio = studio;
+        this.rating = rating;
+    }
+
+    // Constructor 2
+    public Movie(string title, string studio) : this(title, studio, "PG")
+    {
+    }
+
+    // Method GetPG
+    public static Movie[] GetPG(Movie[] movies)
+    {
+        List<Movie> pgMovies = new List<Movie>();
+        foreach (Movie movie in movies)
+        {
+            if (movie.rating == "PG")
+            {
+                pgMovies.Add(movie);
+            }
+        }
+        return pgMovies.ToArray();
+    }
+}
