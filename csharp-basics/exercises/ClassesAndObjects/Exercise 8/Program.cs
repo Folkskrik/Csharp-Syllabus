6e92c8bd0648738d5dbf72176d765afeae2056e7
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-03-05 16:59:01 +0200

    Exercise 8

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SwapPont
+{
+
+    public class Program
+    {
+        public static void SwapPoints(Point p1, Point p2)
+        {
+            int tempX = p1.x;
+            int tempY = p1.y;
+            p1.x = p2.x;
+            p1.y = p2.y;
+            p2.x = tempX;
+            p2.y = tempY;
+        }
+
+        public static void Main()
+        {
+            Point p1 = new Point(5, 2);
+            Point p2 = new Point(-3, 6);
+            SwapPoints(p1, p2);
+            Console.WriteLine("(" + p1.x + ", " + p1.y + ")");
+            Console.WriteLine("(" + p2.x + ", " + p2.y + ")");
+        }
+    }
+}
