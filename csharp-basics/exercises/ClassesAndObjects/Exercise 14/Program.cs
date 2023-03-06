c80aa5307cdd6f77dd3cc69daf5cc91380d26c5c
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-03-06 02:57:31 +0200

    Exercise 14

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 14/Program.cs
@@ -0,0 +1,12 @@
+﻿using Exercise_14;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Date.WeekdayInDutch(1970, 9, 21));
+        Console.WriteLine(Date.WeekdayInDutch(1945, 9, 2));
+        Console.WriteLine(Date.WeekdayInDutch(2001, 9, 11));
+    }
+}
