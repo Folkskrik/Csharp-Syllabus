d20928e6b4ed8d033ff7c4ad01000d1e051e6853
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-03-07 23:02:21 +0200

    Exercise 4

---
+++ csharp-basics/exercises/Collections/Exercise 4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var names = new HashSet<string>();
+
+        while (true)
+        {
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                break;
+            }
+
+            names.Add(name);
+        }
+
+        Console.WriteLine("Unique name list contains: " + string.Join(" ", names));
+    }
+}
