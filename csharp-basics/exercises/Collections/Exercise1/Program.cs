ba8b190b3ac079ae8f2c676aa62b3a3b9300d16e
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-03-07 20:25:00 +0200

    Ex1

--- csharp-basics/exercises/Collections/Exercise1/Program.cs
+++ csharp-basics/exercises/Collections/Exercise1/Program.cs
@@ -28,39 +28,6 @@ namespace Exercise1
             {
                 Console.WriteLine(brand);
             }
-
-            //todo - replace array with a Dictionary (use brand as key and origination as value) and print out the results
-
-            Dictionary<string, string> dictionary = new Dictionary<string, string>();
-
-            foreach (string brand in array)
-            {
-                switch (brand)
-                {
-                    case "Audi":
-                        dictionary.Add(brand, "Germany");
-                        break;
-                    case "BMW":
-                        dictionary.Add(brand, "Germany");
-                        break;
-                    case "Honda":
-                        dictionary.Add(brand, "Japan");
-                        break;
-                    case "Mercedes":
-                        dictionary.Add(brand, "Germany");
-                        break;
-                    case "VolksWagen":
-                        dictionary.Add(brand, "Germany");
-                        break;
-                    case "Tesla":
-                        dictionary.Add(brand, "USA");
-                        break;
-                }
-            }
-            foreach (KeyValuePair<string, string> kvp in dictionary)
-            {
-                Console.WriteLine("{0}: {1}", kvp.Key, kvp.Value);
-            }
         }
     }
 }
