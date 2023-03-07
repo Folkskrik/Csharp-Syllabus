a6ff2730fceea309aab120a8516e6d758abc5977
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-03-07 20:24:20 +0200

    Exercise 1

---
+++ csharp-basics/exercises/Collections/Ex1 Dictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] array = { "Audi", "BMW", "Honda", "Mercedes", "VolksWagen", "Mercedes", "Tesla" };
+Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+foreach (string brand in array)
+{
+    switch (brand)
+    {
+        case "Audi":
+        case "BMW":
+        case "Mercedes":
+        case "VolksWagen":
+            dictionary.TryAdd(brand, "Germany");
+            break;
+        case "Honda":
+            dictionary.TryAdd(brand, "Japan");
+            break;
+        case "Tesla":
+            dictionary.TryAdd(brand, "USA");
+            break;
+        default:
+            Console.WriteLine($"Unknown brand: {brand}");
+            break;
+    }
+}
+
+foreach (KeyValuePair<string, string> kvp in dictionary)
+{
+    Console.WriteLine("{0}: {1}", kvp.Key, kvp.Value);
+}
--- csharp-basics/exercises/Collections/Exercise1/Program.cs
+++ csharp-basics/exercises/Collections/Exercise1/Program.cs
@@ -8,15 +8,6 @@ namespace Exercise1
 {
     class Program
     {
-        /**
-           * Origination:
-           * Audi -> Germany
-           * BMW -> Germany
-           * Honda -> Japan
-           * Mercedes -> Germany
-           * VolksWagen -> Germany
-           * Tesla -> USA
-           */
 
         private static void Main(string[] args)
         {
@@ -24,9 +15,52 @@ namespace Exercise1
 
             //todo - replace array with an List and print out the results
 
+            List<string> list = new List<string>();
+            list.AddRange(array);
+
+            list.ForEach(Console.WriteLine);
+
             //todo - replace array with a HashSet and print out the results
 
+            HashSet<string> set = new HashSet<string>(array);
+
+            foreach (string brand in set)
+            {
+                Console.WriteLine(brand);
+            }
+
             //todo - replace array with a Dictionary (use brand as key and origination as value) and print out the results
+
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            foreach (string brand in array)
+            {
+                switch (brand)
+                {
+                    case "Audi":
+                        dictionary.Add(brand, "Germany");
+                        break;
+                    case "BMW":
+                        dictionary.Add(brand, "Germany");
+                        break;
+                    case "Honda":
+                        dictionary.Add(brand, "Japan");
+                        break;
+                    case "Mercedes":
+                        dictionary.Add(brand, "Germany");
+                        break;
+                    case "VolksWagen":
+                        dictionary.Add(brand, "Germany");
+                        break;
+                    case "Tesla":
+                        dictionary.Add(brand, "USA");
+                        break;
+                }
+            }
+            foreach (KeyValuePair<string, string> kvp in dictionary)
+            {
+                Console.WriteLine("{0}: {1}", kvp.Key, kvp.Value);
+            }
         }
     }
 }
