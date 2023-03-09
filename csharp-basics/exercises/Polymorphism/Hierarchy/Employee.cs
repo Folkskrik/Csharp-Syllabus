5d2301c20fc89b74733ea2bbb87843cd9e37b693
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-03-09 22:17:49 +0200

    Hierarchy

---
+++ csharp-basics/exercises/Polymorphism/Hierarchy/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hierarchy
+{
+    public class Employee : Person
+    {
+        private string jobTitle;
+
+        public Employee(string jobTitle)
+        {
+            jobTitle = jobTitle;
+        }
+
+        public string JobTitle
+        {
+            get { return jobTitle; }
+            set { jobTitle = value; }
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("Job title: {0}", jobTitle);
+        }
+    }
+}
---
+++ csharp-basics/exercises/Polymorphism/Hierarchy/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hierarchy
+{
+    public class Student : Person
+    {
+        private double gpa;
+
+        public Student(double gpa)
+        {
+            this.gpa = gpa;
+        }
+
+        public double GPA
+        {
+            get { return gpa; }
+            set { gpa = value; }
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("GPA: {0}", gpa);
+        }
+    }
+}
