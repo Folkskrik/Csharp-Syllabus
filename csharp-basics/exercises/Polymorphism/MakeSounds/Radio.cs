a75856085ba5440008f1355d846fae59a6b45a47
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-03-09 21:26:06 +0200

    MakeSounds

---
+++ csharp-basics/exercises/Polymorphism/MakeSounds/Radio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakeSounds
+{
+    public class Radio : ISound
+    {
+        private string sound;
+
+        public Radio(string sound)
+        {
+            this.sound = sound;
+        }
+
+        public void PlaySound()
+        {
+            Console.WriteLine(sound);
+        }
+    }
+}
