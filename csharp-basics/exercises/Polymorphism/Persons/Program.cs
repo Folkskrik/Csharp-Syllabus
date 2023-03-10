0159751d7db6781e4e2c978cdae96656c1a232ce
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-03-10 19:08:46 +0200

    Revert "Gad"
    This reverts commit 9c0b083958e033c8439a43d647a02583d482775b.

--- csharp-basics/exercises/Polymorphism/Persons/Program.cs
+++ csharp-basics/exercises/Polymorphism/Persons/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-namespace Hierarchy
+namespace Persons
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             * - Create an instant of class Student and an instant of class Employee with proper values for the attributes.
+             * - Display the content of each class using Display() method.
+             */
         }
     }
 }
