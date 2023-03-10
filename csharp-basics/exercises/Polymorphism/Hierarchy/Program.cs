9c0b083958e033c8439a43d647a02583d482775b
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-03-10 19:05:22 +0200

    Gad

--- csharp-basics/exercises/Polymorphism/Hierarchy/Program.cs
+++ csharp-basics/exercises/Polymorphism/Hierarchy/Program.cs
@@ -1,31 +1,15 @@
 ﻿using System;
 
-namespace Hierarchy
+namespace Persons
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Student student = new Student(4.9);
-            student.FirstName = "Peter";
-            student.LastName = "Parker";
-            student.Adress = "20 Ingram St";
-            student.Id = 1;
-
-            Console.WriteLine("Student:");
-            student.Display();
-            Console.WriteLine();
-
-            Employee employee = new Employee("The Boss");
-            employee.FirstName = "J. Jonah";
-            employee.LastName = "Jameson";
-            employee.Adress = "39th Street and Second Avenue";
-            employee.Id = 2;
-            employee.JobTitle = "CEO";
-
-            Console.WriteLine("Employee:");
-            employee.Display();
-
+            /*
+             * - Create an instant of class Student and an instant of class Employee with proper values for the attributes.
+             * - Display the content of each class using Display() method.
+             */
         }
     }
 }
