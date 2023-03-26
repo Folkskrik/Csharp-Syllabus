5c149530682197427d41217823f9e5c73da27632
Aleksandrs Keišs <aleksandrs.keiss@outlook.lv>
2023-03-26 23:59:00 +0300

    ScooterRental

---
+++ csharp-basics/exercises/Tests/ScooterRental.Test/RentalCompanyTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Moq.AutoMock;
+
+namespace ScooterRental.Test
+{
+    public class RentalCompanyTests
+    {
+        private IRentalCompany _company;
+        private const string CompanyName = "test";
+        private AutoMocker _mocker;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mocker = new AutoMocker();
+            _company = new RentalCompany(CompanyName, _mocker.GetMock<IScooterService>().Object);
+        }
+
+        [Test]
+        public void CreateRentalCompany_TestAsNameProvided_NameShouldBeTest()
+        {
+            _company.Name.Should().Be(CompanyName);
+        }
+
+        [Test]
+        public void StartRent_ValidIdProvided_ScooterIsRented()
+        {
+            var scooter = new Scooter("1", 1m);
+            var mock = _mocker.GetMock<IScooterService>();
+            mock.Setup(x => x.GetScooterById("1")).Returns(scooter);
+
+            _company.StartRent("1");
+
+            scooter.IsRented.Should().BeTrue();
+        }
+
+        [Test]
+
+        public void StartRent_InvalidIdProvided_ThrowsScooterNotFoundException()
+        {
+
+        }
+    }
+}
---
+++ csharp-basics/exercises/Tests/ScooterRental/RentalCompany.cs
@@ -0,0 +1,28 @@
+﻿namespace ScooterRental
+{
+    public class RentalCompany : IRentalCompany
+    {
+        private readonly IScooterService _scooterService;
+        public RentalCompany(string name, IScooterService scooterService)
+        {
+            Name = name;
+            _scooterService = scooterService;
+        }
+        public string Name { get; }
+        public void StartRent(string id)
+        {
+            var scooter = _scooterService.GetScooterById(id);
+            scooter.IsRented = true;
+        }
+
+        public decimal EndRent(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CalculateIncome(int? year, bool includeNotCompletedRentals)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
